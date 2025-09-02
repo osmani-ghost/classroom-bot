be9dc59a524a7699b2155513f89e1fa6b2ee2516
Sabbir Hossain Osmani <sabbirosmani04@gmail.com>
2025-09-02 12:34:48 +0600

    Add Messenger webhook handler logic

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     const VERIFY_TOKEN = process.env.MESSENGER_VERIFY_TOKEN;
 
-    console.log("👉 FB sent token:", req.query["hub.verify_token"]);
-    console.log("👉 Our VERIFY_TOKEN from env:", VERIFY_TOKEN);
+    console.log("FB sent token:", req.query["hub.verify_token"]);
+    console.log("Our VERIFY_TOKEN from env:", VERIFY_TOKEN);
 
     const mode = req.query["hub.mode"];
     const token = req.query["hub.verify_token"];
